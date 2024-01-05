1d11f6184172561b9cb8502bb0704f6506f52288
Denis Aksenov <BLIHOK@mail.ru>
2024-01-05 17:51:39 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,90 @@
+fun main(args: Array<String>) {
+//    val list = mutableListOf(1, 2, 3)
+//    println(list)
+//
+//    list.swap(0, 2)
+//    println(list)
+//
+//    val strList = mutableListOf("One", "Two", "Three")
+//    println(strList)
+//    strList.swap(0,2)
+//    println(strList)
+//}
+//
+//
+//fun <E>MutableList<E>.swap(index1: Int, index2: Int){
+//    val e1 = this[index1]
+//    val e1 = get(index1) // можно заменить
+//    val e2 = this[index2]
+//    val e2 = get(index2)
+//    this[index1] = e2
+//    this[index2] = e1
+
+val list = mutableListOf(T)
+    list.noop()
+
+val collection :MutableCollection<Post> = list
+    collection.noop()
+
+
+
+
+
+    //////////////////////////////////////////////////////////
+//        val hello: String = "hello world"
+//        println(hello.wordCount('l'))   // 3
+//        println(hello.wordCount('v'))   // 2
+//        println(4.square())             // 16
+//        println(6.square())             // 36
+//    }
+//
+//    fun String.wordCount(c: Char) : Int {
+//        var count = 0
+//        for(n in this) {
+//            if(n == c) count++
+//        }
+//        return count
+//    }
+//    fun Int.square(): Int {
+//        return this * this
+//    }
+
+/////////////////////////////////////////////////////
+//    val sum = {x:Int, y:Int -> x + y}
+//
+//    val a = sum(2, 3)   // 5
+//    val b = sum(4, 5)   // 9
+//    println("a=$a  b=$b")
+//}
+
+//    val printer = {message: String -> println(message)}
+//    printer("Hello")
+//    printer("Good Bye")
+//}
+//    val sum = { x: Int, y: Int ->
+//        val v = x + y
+//        println(v)
+//    }
+//    sum(1, 5)
+
+
+//    val numbers = listOf(1, 2, 3, 4, 5)
+//    val evenNumbers = numbers.filter(fun(n) = n % 2 == 0)
+//    println(evenNumbers)
+
+//    var s = { s: String -> println(s) }
+//    s("He")
+
+
+//    val cats = listOf("Barsik", "Murzik", "Ryzhik")
+//    cats.forEach { println(it) }
+
+//    val some: String = "Some"
+//
+//    some.land()
+//
+//
+//}
+//fun <T> T.land()  {
+//    println(this)
+}
